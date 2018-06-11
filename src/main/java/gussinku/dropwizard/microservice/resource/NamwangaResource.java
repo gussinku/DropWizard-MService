612dfdb5cfa618d5538bdd5a397abdc984bccfad
gussinku <gussinku@student.gu.se>
2018-06-11 20:54:18 +0200

    get methd changed to arraylist type
    Signed-off-by: gussinku <gussinku@student.gu.se>

--- src/main/java/gussinku/dropwizard/microservice/resource/NamwangaResource.java
+++ src/main/java/gussinku/dropwizard/microservice/resource/NamwangaResource.java
@@ -6,16 +6,20 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Path("")
 @Produces(MediaType.APPLICATION_JSON)
 public class NamwangaResource {
-    private String[] mwakata = {"Kundananji, Izukanji,Tamikanji,Mukundwe"};
+    private ArrayList<String> mwakata =
+            new ArrayList<>(Arrays.asList("Kundananji, Izukanji,Tamikanji,Mukundwe"));
+
 
     @GET
     @Timed
     @Path("/namwanga-name")
-    public String[] displayName() {
+    public ArrayList<String> getNawangaName() {
         return this.mwakata;
     }
 
